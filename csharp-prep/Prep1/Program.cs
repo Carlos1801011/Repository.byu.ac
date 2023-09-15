31e524b5a03123de7cbf9a2491d5a6bda782d893
carlos1801011 <cha21074@byui.edu>
2023-09-15 10:37:18 -0600

    Update Homework

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,13 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.Write("What is your first name? > ");
+        string firstline = Console.ReadLine();
+
+        Console.Write("What is your last name? > ");
+        string secondline = Console.ReadLine();
+
+        Console.WriteLine($"Your name is {secondline}, {firstline} {secondline}.");
+    
     }
 }
