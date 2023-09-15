65e324e35485be31fe916bd60c9dabb426164ba2
carlos1801011 <cha21074@byui.edu>
2023-09-15 11:01:29 -0600

    Prep2 ready

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,42 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage right now> > ");
+        string response = Console.ReadLine();
+        int percenta = int.Parse(response);
+
+        string grade = "";
+
+        if (percenta >= 90)
+        {
+            grade = "A";
+        }
+        else if (percenta >= 80)
+        {
+            grade = "B";
+        }
+        else if (percenta >= 70)
+        {
+            grade = "C";
+        }
+        else if (percenta >= 60)
+        {
+            grade = "D";
+        }
+        else
+        {
+            grade = "F";
+        }
+
+        Console.WriteLine($"Your Grade in this moment is : {grade}");
+
+        if (percenta >= 70)
+        {
+            Console.WriteLine("You passed :))");   
+        }
+        else
+        {
+            Console.WriteLine("Do it better another time but don't give up. :)");
+        }
     }
 }
