dd25c46f3606ab5fe4106639357ed9c98571426e
carlos1801011 <cha21074@byui.edu>
2023-09-16 19:22:28 -0600

    Magic number online

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,45 @@
 using System;
+using System.Timers;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        bool playAgain1 = true;
+
+        while(playAgain1)
+        {
+        Random randomG = new Random();
+        int Mnumber = randomG.Next(1,2);
+        int Suppose = -1;
+
+        while (Suppose != Mnumber)
+        {
+            Console.Write("What is your Suppose Number? > ");
+            Suppose = int.Parse(Console.ReadLine());
+
+            if ( Mnumber >= Suppose)
+            {
+                Console.WriteLine("Very Higher");
+            }
+            else if (Mnumber < Suppose)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("You Got it!");
+            }
+        }
+        Console.Write("Do you want to play again? (yes or no) :) > ");
+        string playAgainIn = Console.ReadLine();
+
+        if (playAgainIn.ToLower() != "yes")
+        {
+            playAgain1 = false;
+            
+        }
+        Console.WriteLine("Thanks you for playing The Magic Number :)");
     }
+}
 }
