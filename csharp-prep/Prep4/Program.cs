da4a6e26e1ed9957da5060a6c8c1971d59ef231b
carlos1801011 <cha21074@byui.edu>
2023-09-16 20:14:05 -0600

    With positive number

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,60 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+        
+
+        int user = -1;
+        while (user != 0)
+        {
+            Console.Write("Enter a number (0 to quit) if you finish: ");
+            
+            string userResponse = Console.ReadLine();
+            user = int.Parse(userResponse);
+            
+
+            if (user != 0)
+            {
+                numbers.Add(user);
+            }
+        }
+
+
+        int sum = 0;
+        foreach (int number in numbers)
+        {
+            sum += number;
+        }
+
+        Console.WriteLine($"The sum is: {sum}");
+
+ 
+        int max = numbers[0];
+
+        foreach (int number in numbers)
+        {
+            if (number > max)
+            {
+                max = number;
+            }
+        }
+
+        Console.WriteLine($"The max is: {max}");
+
+        int minPositive = int.MaxValue;
+
+        foreach (int num in numbers)
+        {
+        if (num > 0 && num < minPositive)
+        {
+            minPositive = num;
+        }
+}
+
+Console.WriteLine("The smallest positive number is: " + minPositive);
     }
 }
